5f3939cc4c8ff9ee1f2f522ce76f0d48a52ceeab
Jeff Richardson <jeffreywebdevelopment@gmail.com>
2015-05-15 20:27:34 -0500

    passing tests

--- src/AxShorten.js
+++ src/AxShorten.js
@@ -1,8 +1,18 @@
-var AxShorten = function AxShorten(options) {
-  this.elem = options.elem;
-  this.characterLimit = options.count || 100;
+AxShorten = (function() {
+  'use strict';
 
-  /* We need to grab the length of the element content */
-  var content = this.elem.textContent;
-  var contentLength = this.elem.textContent.length;
-}
+  var AC = function (options) {
+    this.elem = options.elem;
+    this.characterLimit = options.count || 100;
+
+    /* We need to grab the length of the element content */
+    var content = this.elem.textContent;
+    var contentLength = this.elem.textContent.length;
+
+    this.version = "0.0.1";
+    return this;
+  };
+
+  return AC;
+
+}());
