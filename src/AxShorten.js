0c9be3d30802c176ba92f53a2bec5cc299b1c6bd
Jeff Richardson <jeffreywebdevelopment@gmail.com>
2015-05-17 12:24:07 -0500

    adjust spacing on AxShorten

--- src/AxShorten.js
+++ src/AxShorten.js
@@ -7,21 +7,21 @@ AxShorten = (function() {
   //-------------------------------
 
   var AC = function (options) {
-  	this.elem = options.elem;
+    this.elem = options.elem;
     this.characterLimit = options.characterLimit || 100;
     this.originalContent = '';
     this.contentLength = 0;
     this.newContent = '';
-    this.charactersToBeShown = ''; 
-   	init.call(this);
+    this.charactersToBeShown = '';
+    init.call(this);
   };
 
   //-------------------------------
   //  Private API *?
   //-------------------------------
 
-  var isInTag = false; 
-  var currentSizeOfCharactersToBeShown = 0; 
+  var isInTag = false;
+  var currentSizeOfCharactersToBeShown = 0;
   var openTags = []; // Stack for opened tags, so I can close them later
   var tagName = null;
 
@@ -34,142 +34,115 @@ AxShorten = (function() {
 
   var checkLength = function(contentLength) {
     if (this.contentLength > this.characterLimit) {
-        createNewContent.call(this);
+      createNewContent.call(this);
     }
   };
 
   var createNewContent = function() {
-  	this.newContent = this.originalContent.substr(0, this.characterLimit);
-  	parseContent.call(this);
+    this.newContent = this.originalContent.substr(0, this.characterLimit);
+    parseContent.call(this);
   };
 
   var parseContent = function() {
-  	if (this.newContent.indexOf('<') >= 0) {
-  		parseContentForHtml.call(this);
-  	} else {
-  		parseContentForNonHtml.call(this);
-  	}
+    if (this.newContent.indexOf('<') >= 0) {
+      parseContentForHtml.call(this);
+    } else {
+      parseContentForNonHtml.call(this);
+    }
   };
 
   var parseContentForNonHtml = function() {
-  	this.charactersToBeShown = this.originalContent.substr(0, this.characterLimit);
-  	returnShortContent.call(this);
+    this.charactersToBeShown = this.originalContent.substr(0, this.characterLimit);
+    returnShortContent.call(this);
   };
 
   var parseContentForHtml = function() {
-  	var i = null,
-  	    r = null;
+    parseAllHtmlContent.call(this);
+    returnShortContent.call(this);
+  };
 
-	for (i = 0, r = 0; r <= this.characterLimit; i++) {
-		formatShortContent.call(this, i);
+  var parseAllHtmlContent = function() {
+    var i = null,
+        r = null;
 
-	    if (isInTag && this.originalContent.charAt(i) == '>') {
-	    	isInTag = false;
-	    }
+    for (i = 0, r = 0; r <= this.characterLimit; i++) {
+      formatShortContent.call(this, i);
 
-	    if (isInTag) { this.charactersToBeShown += this.originalContent.charAt(i); } // Add tag name chars to the result
-	    else {
-	        r++;
-	        if (currentSizeOfCharactersToBeShown <= this.characterLimit) {
-	            this.charactersToBeShown += this.originalContent.charAt(i); // Fix to ie 7 not allowing you to reference string characters using the []
-	            currentSizeOfCharactersToBeShown++;
-	        } else {
-	        	// Now I have the characters needed
-	            checkOpenTags.call(this);
-	            break;
-	       }
-	       
-	    }
-	} // end for loop
+      checkForEndingHtmlTag.call(this, i);
 
+      if (isInTag) {
+        this.charactersToBeShown += this.originalContent.charAt(i);
+      } // Add tag name chars to the result
+      else {
+        r++;
+        if (currentSizeOfCharactersToBeShown <= this.characterLimit) {
+          this.charactersToBeShown += this.originalContent.charAt(i); // Fix to ie 7 not allowing you to reference string characters using the []
+          currentSizeOfCharactersToBeShown++;
+	} else {
+          // Now I have the characters needed
+	  checkOpenTags.call(this);
+	  break;
+        }
+      }
+    } // end for loop
+  };
 
-	returnShortContent.call(this);
-
+  var checkForEndingHtmlTag = function(indexOfCharacter) {
+    if (isInTag && this.originalContent.charAt(indexOfCharacter) == '>') {
+      isInTag = false;
+    }
   };
 
   var checkOpenTags = function() {
-  	if (openTags.length > 0) {
-  		closeOpenTags.call(this);
-  	}
+    if (openTags.length > 0) {
+      closeOpenTags.call(this);
+    }
   };
 
   var closeOpenTags = function() {
-  	for (var j = 0; j < openTags.length; j++) {
-		//console.log('Cierro tag ' + openTags[j]);
-	    this.newContent+= '</' + openTags[j] + '>'; // Close all tags that were opened
+    for (var j = 0; j < openTags.length; j++) {
+      //console.log('Cierro tag ' + openTags[j]);
+      this.newContent+= '</' + openTags[j] + '>'; // Close all tags that were opened
 
-	    // You could shift the tag from the stack to check if you end with an empty stack, that means you have closed all open tags
-	}
+     // You could shift the tag from the stack to check if you end with an empty stack, that means you have closed all open tags
+    }
   };
 
   var formatShortContent = function(indexOfCharacter) {
-	if (this.newContent[indexOfCharacter] == '<' && !isInTag) {
-		isInTag = true;
+    if (this.newContent[indexOfCharacter] == '<' && !isInTag) {
+      isInTag = true;
 
-	    // This could be "tag" or "/tag"
-	    tagName = this.originalContent.substring(indexOfCharacter + 1, this.originalContent.indexOf('>', indexOfCharacter));
+      // This could be "tag" or "/tag"
+      tagName = this.originalContent.substring(indexOfCharacter + 1, this.originalContent.indexOf('>', indexOfCharacter));
 
-	    // If its a closing tag
-	    if (tagName[0] == '/') {
+    // If its a closing tag
+      if (tagName[0] == '/') {
 
-
-	       	if (tagName != '/' + openTags[0]) {
-	        	this.errMsg = 'ERROR en HTML: the top of the stack should be the tag that closes';
-	        } else {
-	            openTags.shift(); // Pops the last tag from the open tag stack (the tag is closed in the retult HTML!)
-	    	}
-
-	    } else {
-	        	// There are some nasty tags that don't have a close tag like <br/>
-	        if (tagName.toLowerCase() != 'br') {
-	        	openTags.unshift(tagName); // Add to start the name of the tag that opens
-	       	}
-	   	}
+	if (tagName != '/' + openTags[0]) {
+	  this.errMsg = 'ERROR en HTML: the top of the stack should be the tag that closes';
+	} else {
+	  openTags.shift(); // Pops the last tag from the open tag stack (the tag is closed in the retult HTML!)
 	}
+      } else {
+	// There are some nasty tags that don't have a close tag like <br/>
+	if (tagName.toLowerCase() != 'br') {
+	  openTags.unshift(tagName); // Add to start the name of the tag that opens
+	}
+      }
+    }
   };
 
 
-
-
-
-
   var returnShortContent = function() {
-  	var html = '<div class="shortcontent">' + this.charactersToBeShown +
+    var html = '<div class="shortcontent">' + this.charactersToBeShown +
                     '...</div>';
 
 
      this.elem.outerHTML = html;
   }
 
-
-
   return AC;
 
 }());
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
