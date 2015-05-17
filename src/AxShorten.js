eb5b9c1326b05c4758612f57df85d7edb12a7398
Jeff Richardson <jeffreywebdevelopment@gmail.com>
2015-05-17 00:49:39 -0500

    Finished basic verison added new tests

--- src/AxShorten.js
+++ src/AxShorten.js
@@ -8,9 +8,11 @@ AxShorten = (function() {
 
   var AC = function (options) {
   	this.elem = options.elem;
-    this.characterLimit = options.count || 100;
+    this.characterLimit = options.characterLimit || 100;
     this.originalContent = '';
     this.contentLength = 0;
+    this.newContent = '';
+    this.charactersToBeShown = ''; 
    	init.call(this);
   };
 
@@ -19,7 +21,6 @@ AxShorten = (function() {
   //-------------------------------
 
   var isInTag = false; 
-  var charactersToBeShown = ''; 
   var currentSizeOfCharactersToBeShown = 0; 
   var openTags = []; // Stack for opened tags, so I can close them later
   var tagName = null;
@@ -28,105 +29,146 @@ AxShorten = (function() {
   var init = function() {
     this.originalContent = this.elem.innerHTML;
     this.contentLength = this.elem.innerHTML.length;
-    /*checkLength.call(this, contentLength); */
+    checkLength.call(this, this.contentLength);
   };
 
   var checkLength = function(contentLength) {
-    if (contentLength > this.characterLimit) {
-       this.newContent = this.originalContent.substr(0, this.characterLimit);
-       parseContent.call(this);
+    if (this.contentLength > this.characterLimit) {
+        createNewContent.call(this);
     };
+  };
+
+  var createNewContent = function() {
+  	this.newContent = this.originalContent.substr(0, this.characterLimit);
+  	parseContent.call(this);
   };
 
   var parseContent = function() {
   	if (this.newContent.indexOf('<') >= 0) {
   		parseContentForHtml.call(this);
+  	} else {
+  		parseContentForNonHtml.call(this);
   	}
+  };
+
+  var parseContentForNonHtml = function() {
+  	this.charactersToBeShown = this.originalContent.substr(0, this.characterLimit);
+  	returnShortContent.call(this);
   }
 
   var parseContentForHtml = function() {
 
 
 	for (var i = 0, r = 0; r <= this.characterLimit; i++) {
-	                        if (this.newContent[i] == '<' && !isInTag) {
-	                            isInTag = true;
+		formatShortContent.call(this, i);
 
-	                            // This could be "tag" or "/tag"
-	                            tagName = this.newContent.substring(i + 1, this.newContent.indexOf('>', i));
+	    if (isInTag && this.originalContent.charAt(i) == '>') {
+	    	isInTag = false;
+	    }
 
-	                            // If its a closing tag
-	                            if (tagName[0] == '/') {
-
-
-	                                if (tagName != '/' + openTags[0]) {
-	                                    this.errMsg = 'ERROR en HTML: the top of the stack should be the tag that closes';
-	                                } else {
-	                                    openTags.shift(); // Pops the last tag from the open tag stack (the tag is closed in the retult HTML!)
-	                                }
-
-	                            } else {
-	                                // There are some nasty tags that don't have a close tag like <br/>
-	                                if (tagName.toLowerCase() != 'br') {
-	                                    openTags.unshift(tagName); // Add to start the name of the tag that opens
-	                                }
-	                            }
-	                        }
-	                        if (isInTag && this.newContent[i] == '>') {
-	                            isInTag = false;
-	                        }
-
-	                        if (isInTag) { this.charactersToBeShown += this.newContent.charAt(i); } // Add tag name chars to the result
-	                        else {
-	                            r++;
-	                            if (currentSizeOfCharactersToBeShown <= this.charactersToBeShown) {
-	                                this.charactersToBeShown += this.newContent.charAt(i); // Fix to ie 7 not allowing you to reference string characters using the []
-	                                currentSizeOfCharactersToBeShown++;
-	                            } else // Now I have the characters needed
-	                            {
-	                                if (openTags.length > 0) // I have unclosed tags
-	                                {
-	                                    //console.log('They were open tags');
-	                                    //console.log(openTags);
-	                                    for (j = 0; j < openTags.length; j++) {
-	                                        //console.log('Cierro tag ' + openTags[j]);
-	                                        charactersToBeShown += '</' + openTags[j] + '>'; // Close all tags that were opened
-
-	                                        // You could shift the tag from the stack to check if you end with an empty stack, that means you have closed all open tags
-	                                    }
-	                                    break;
-	                                }
-	                            }
-	                        }
-	}
+	    if (isInTag) { this.charactersToBeShown += this.originalContent.charAt(i); } // Add tag name chars to the result
+	    else {
+	        r++;
+	        if (currentSizeOfCharactersToBeShown <= this.characterLimit) {
+	            this.charactersToBeShown += this.originalContent.charAt(i); // Fix to ie 7 not allowing you to reference string characters using the []
+	            currentSizeOfCharactersToBeShown++;
+	        } else {
+	        	// Now I have the characters needed
+	            checkOpenTags.call(this);
+	            break;
+	       }
+	       
+	    }
+	} // end for loop
 
 
+	returnShortContent.call(this);
 
-	var html = '<div class="shortcontent">' + c +
-                    '</div>';
-
-
-     elem.innerHTML = html;
-
-  }
-
-
-  //-----------------------------
-  // OpenTagStack
-  // -----------------------------
-  var OpenTagStack = function() {
-    var openTags = null;
-
-    function initialize() {
-      openTags = [];
-    };
-
-
-
-    initialize();
   };
+
+  var checkOpenTags = function() {
+  	if (openTags.length > 0) {
+  		closeOpenTags.call(this);
+  	}
+  };
+
+  var closeOpenTags = function() {
+  	for (var j = 0; j < openTags.length; j++) {
+		//console.log('Cierro tag ' + openTags[j]);
+	    this.newContent+= '</' + openTags[j] + '>'; // Close all tags that were opened
+
+	    // You could shift the tag from the stack to check if you end with an empty stack, that means you have closed all open tags
+	}
+  };
+
+  var formatShortContent = function(indexOfCharacter) {
+	if (this.newContent[indexOfCharacter] == '<' && !isInTag) {
+		isInTag = true;
+
+	    // This could be "tag" or "/tag"
+	    tagName = this.originalContent.substring(indexOfCharacter + 1, this.originalContent.indexOf('>', indexOfCharacter));
+
+	    // If its a closing tag
+	    if (tagName[0] == '/') {
+
+
+	       	if (tagName != '/' + openTags[0]) {
+	        	this.errMsg = 'ERROR en HTML: the top of the stack should be the tag that closes';
+	        } else {
+	            openTags.shift(); // Pops the last tag from the open tag stack (the tag is closed in the retult HTML!)
+	    	}
+
+	    } else {
+	        	// There are some nasty tags that don't have a close tag like <br/>
+	        if (tagName.toLowerCase() != 'br') {
+	        	openTags.unshift(tagName); // Add to start the name of the tag that opens
+	       	}
+	   	}
+	}
+  };
+
+
+
+
+
+
+  var returnShortContent = function() {
+  	var html = '<div class="shortcontent">' + this.charactersToBeShown +
+                    '...</div>';
+
+
+     this.elem.outerHTML = html;
+  }
 
 
 
   return AC;
 
 }());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- tests/tests.js
+++ tests/tests.js
@@ -3,7 +3,7 @@ describe('AxShorten', function() {
   it('should have an element and a character count limit', function() {
      var options = {
            elem: document.querySelector('.testElement'),
-           count: 5
+           characterLimit: 5
      };
      var newElem = new AxShorten(options);
      newElem.should.have.property('elem');
@@ -12,31 +12,61 @@ describe('AxShorten', function() {
 
   it('should have a default character limit', function() {
      var options = {
-           elem: document.querySelector('.testElement')
+           elem: document.querySelector('.testElement1')
      };
 
      var newElem = new AxShorten(options);
      expect(newElem.characterLimit).to.equal(100);
   });
 
-  it('should have access to original content in memory', function() {
+  it('should have a reference to original content in memory', function() {
      var options = {
-           elem: document.querySelector('.testElement')
+           elem: document.querySelector('.testElement2')
      };
 
      var newElem = new AxShorten(options);
-     expect(newElem.originalContent).to.equal(options.elem.textContent);
+     expect(newElem.originalContent).to.equal(options.elem.innerHTML);
   });
 
 
-  it('should have access to original content length in memory', function() {
+  it('should have reference to original content length in memory', function() {
      var options = {
-           elem: document.querySelector('.testElement')
+           elem: document.querySelector('.testElement3')
      };
 
      var newElem = new AxShorten(options);
-     expect(newElem.contentLength).to.equal(options.elem.textContent.length);
+     expect(newElem.contentLength).to.equal(options.elem.innerHTML.length);
   });
 
+
+  it('should have public reference to the new content', function() {
+     var options = {
+           elem: document.querySelector('.testElement4')
+     };
+
+     var newElem = new AxShorten(options);
+     expect(newElem.newContent).to.exist;
+  });
+
+  it('new content should have a length', function() {
+     var options = {
+           elem: document.querySelector('.testElement5')
+     };
+
+     var newElem = new AxShorten(options);
+     expect(newElem.newContent.length).to.equal(newElem.characterLimit);
+  });
+
+  it('should shorten the content of the targeted dom element', function() {
+     var options = {
+           elem: document.querySelector('.testElement6Container')
+     };
+
+     var newElem = new AxShorten(options);
+
+     var elem = document.querySelector('.testElement6')
+
+     expect(elem.innerHTML.length).to.be.below(newElem.characterLimit);
+  });
 
 });
